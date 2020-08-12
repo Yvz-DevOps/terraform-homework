b3f1fd74e1683797ba8d9487b9ecde5e8803405f
Yvz-DevOps <68934275+Yvz-DevOps@users.noreply.github.com>
2020-08-12 01:10:32 -0400

    Update homework.tf

--- homework.tf
+++ homework.tf
@@ -1,1 +1,47 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_iam_group" "developers" {
+  name = "developers"
+  path = "/"
+}
+resource "aws_iam_group" "developers" {
+  name = "Management"
+  path = "/"
+}
+
+resource "aws_iam_group_membership" "developers_team" {
+  name = "developers-group-membership"
+
+  users = [
+    "${aws_iam_user.Tim.name}", "${aws_iam_user.Bob.name}", "${aws_iam_user.Ben.name}",
+  ]
+
+  group = "${aws_iam_group.developers.name}"
+}
+
+resource "aws_iam_group_membership" "Management_team" {
+  name = "Management-group-membership"
+
+  users = [
+    "${aws_iam_user.Bob.name}",
+  ]
+
+  group = "${aws_iam_group.Management.name}"
+}
+
+
+
+
+resource "aws_iam_user" "tim" {
+  name = "Tim"
+}
+resource "aws_iam_user" "tim" {
+  name = "Bob"
+}
+
+resource "aws_iam_user" "tim" {
+  name = "Ben"
+}
 
